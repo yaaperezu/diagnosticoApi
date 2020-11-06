194d2eaad9140c73092f231c84ea36e4c2e08e1c
Yasser Alexander Perez Urariyu (Analista de Aplicaciones Senior) <yaaperez@colsanitas.com>
2020-11-06 07:37:53 -0500

    Test Aut

--- resources/paciente.py
+++ resources/paciente.py
@@ -21,7 +21,7 @@ class PacientesApi(Resource):
 
     def delete(self, documenNumber):
         patiens = Paciente.objects.get(documenNumber=documenNumber).delete()
-        return 'Se elimino el paciente correctamente', 200
+        return 'Se eliminó el paciente correctamente', 200
 
     def get(self, documenNumber):
         try:
